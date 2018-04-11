b6d4085fffdc88704fa0aaa836266c25149300d1
Enis Simsar <enisimsar@gmail.com>
2018-04-12 02:45:44 +0300

    Add functionality of the location tweet endpoint
    - Create a logic file for operations
    - Add new end point, location tweet, to end points.
    - Create a handler for location tweet
    - Add Map Box token to the main handler
    - Fix the map java script code,  run only in home page.

--- KarpuzTwitterApp/handlers/main.py
+++ KarpuzTwitterApp/handlers/main.py
@@ -1,12 +1,18 @@
+from decouple import config
+
 from handlers.base import BaseHandler, TemplateRendering
 
 
 class MainHandler(BaseHandler, TemplateRendering):
+    def data_received(self, chunk):
+        pass
+
     def get(self):
         template = 'index.html'
         variables = {
             'title': "Home",
-            'type': 'home'
+            'type': 'home',
+            'mapbox_token': config("MAPBOX_TOKEN")
         }
         content = self.render_template(template, variables)
         self.write(content)
