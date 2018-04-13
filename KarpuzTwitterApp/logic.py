2e67e3c36066114e47002050f070ed17f36b311e
Muhammed Emin Gure <muhammed.gure@boun.edu.tr>
2018-04-13 10:32:59 +0300

    Added UI and Handler for common followings

--- KarpuzTwitterApp/logic.py
+++ KarpuzTwitterApp/logic.py
@@ -18,7 +18,6 @@ def get_tweets_with_location_and_query(search_params):
 
     return {'response': True, 'tweets': tweets}
 
-<<<<<<< HEAD
 def get_user_timeline(screen_name):
     user_timeline_url = '{}1.1/statuses/user_timeline.json'.format(TwitterService().get_base_url())
 
@@ -40,28 +39,26 @@ def get_user_timeline(screen_name):
     # API directly returns tweets
     tweets = user_timeline_response.json()
     return {'response': True, 'tweets': tweets}
-=======
 
 def get_followings_of_user(search_params):
-    """ Searches all tweets that are in the given location and contains a query string. """
     search_url = '{}1.1/friends/ids.json'.format(TwitterService().get_base_url())
 
     search_response = get(search_url, headers=TwitterService().get_request_headers(), params=search_params)
 
     # If response code different than 200 (means success), then return the error.
     if search_response.status_code != 200:
-        return {'response': False, 'errors': search_response.json()['errors']}
+        print(search_response.json())
+        return {'response': False, 'errors': search_response.json()}
 
     # Subtracts the tweets from the twitter response
     data = search_response.json()
     # print(search_response.json())
     followings = set(data['ids'])
 
-    return followings
+    return {'response': True, 'followings': followings}
 
 
 def get_user_details(search_params):
-    """ Searches all tweets that are in the given location and contains a query string. """
     search_url = '{}1.1/users/lookup.json'.format(TwitterService().get_base_url())
 
     search_response = get(search_url, headers=TwitterService().get_request_headers(), params=search_params)
@@ -76,17 +73,19 @@ def get_user_details(search_params):
     return data
 
 
-def get_common_followings_of_two_user():
+def get_common_followings_of_two_user(search_params):
     params={}
-    params['screen_name'] = 'emin_gure'  # search_params['user_one']
-    followings_user_one = get_followings_of_user(params)
-    params['screen_name'] = '_mehmetemin_'  # search_params['user_two']
-    followings_user_two = get_followings_of_user(params)
+    params['screen_name'] = search_params['user_one']
+    response_user_one = get_followings_of_user(params)
+    if not response_user_one['response']:
+        return {'response': False, 'errors': response_user_one['errors']}
+    followings_user_one = response_user_one['followings']
+    params['screen_name'] = search_params['user_two']
+    response_user_two = get_followings_of_user(params)
+    if not response_user_two['response']:
+        return {'response': False, 'errors': response_user_two['errors']}
+    followings_user_two = response_user_two['followings']
 
     common_followings = followings_user_one.intersection(followings_user_two)
 
     return get_user_details({'user_id': common_followings})
-
-
-get_common_followings_of_two_user()
->>>>>>> Add logic for getting common followings of two user
