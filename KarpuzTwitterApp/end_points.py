4cbcdad77371412ec1701ac4ae79a351b7753101
muhammed emin ayar <eminayar@muhammeds-MacBook-Pro.local>
2018-04-13 03:40:18 +0300

    favorite tweets v1.0

--- KarpuzTwitterApp/end_points.py
+++ KarpuzTwitterApp/end_points.py
@@ -3,11 +3,13 @@ import tornado.web
 from settings import settings
 from handlers.main import MainHandler
 from handlers.locationtweets import LocationTweetsLocationHandler
+from handlers.favoritetweets import FavoriteTweetsHandler
 
 end_point_list = [
     # MAIN
     (r"/", MainHandler),
     (r"/location-tweets", LocationTweetsLocationHandler),
+    (r"/favorite-tweets", FavoriteTweetsHandler),
 
     (r"/(.*)", tornado.web.StaticFileHandler, {'path': settings['static_path']}),
 ]
