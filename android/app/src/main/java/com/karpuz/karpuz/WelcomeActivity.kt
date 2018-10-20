e2ce64aa9020bc3caebd044e6487015e3b7f0488
Bahadir Oncel <bahadir.oencel@fretello.com>
2018-10-20 20:04:38 +0300

    initial project setup

---
+++ android/app/src/main/java/com/karpuz/karpuz/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.karpuz.karpuz
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
