1947090249b3018c33727fab034d0fb76c798cd7
Bahadir Oncel <bahadir.onc@gmail.com>
2018-10-20 20:35:02 +0300

    add click handlers for login and signup buttons

--- android/app/src/main/java/com/karpuz/karpuz/WelcomeActivity.kt
+++ android/app/src/main/java/com/karpuz/karpuz/WelcomeActivity.kt
@@ -2,11 +2,20 @@ package com.karpuz.karpuz
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+    }
+
+    fun loginClicked(button: View) {
+
+    }
+
+    fun signupClicked(button: View) {
+
     }
 }
