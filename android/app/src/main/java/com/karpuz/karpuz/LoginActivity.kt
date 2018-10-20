96b845aa0c7d91599920362b11eee96bd2957254
Bahadir Oncel <bahadir.onc@gmail.com>
2018-10-20 20:47:05 +0300

    add login activity

---
+++ android/app/src/main/java/com/karpuz/karpuz/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.karpuz.karpuz
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun signInClicked(button: View) {
+
+    }
+
+    fun forgotPasswordClicked(button: View) {
+
+    }
+}
