410024afc2f376398b420451ea2135a4cc45c02e
Enis Simsar <enis.simsar@boun.edu.tr>
2018-10-25 18:37:47 +0300

    Add mongodb connection.

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import mongoengine
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -70,16 +72,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'api.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+mongoengine.connect(
+    config('MONGODB_DB'),
+    username=config('MONGODB_USER'),
+    password=config('MONGODB_PASSWORD'),
+    host='mongodb',
+    port=config('MONGODB_PORT', cast=int),
+    authentication_source='admin',
+    connect=False
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
