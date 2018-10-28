2168eb5fc86d90af8df05fa8cc481f70b4a571bc
alpkaanusen <ausen96@hotmail.com>
2018-10-28 19:49:28 +0300

    Get profile added

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -4,5 +4,8 @@ from django.conf.urls import url
 urlpatterns = [
     url(r'^auth/login', views.login, name="login"),
     url(r'^auth/register', views.register, name="register"),
-    url(r'^auth/logout', views.logout, name="logout")
+    url(r'^auth/logout', views.logout, name="logout"),
+    url(r'^profile/(?P<user_id>[a-z0-9,]*)/$', views.get_user, name="get_user"),
+    url(r'^profile', views.get_current_user, name="get_current_user"),
+    url(r'^profile/update', views.update_user, name="update_user")
 ]
--- backend/user/views.py
+++ backend/user/views.py
@@ -6,6 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 from . import authentication
 import json
 from .models import User, DoesNotExist
+from datetime import datetime
 
 
 def hash_password(password):
@@ -16,6 +17,34 @@ def hash_password(password):
 def check_password(hashed_password, user_password):
     password, salt = hashed_password.split(':')
     return password == hashlib.sha256(salt.encode() + user_password.encode()).hexdigest()
+
+
+def user_json(user):
+    obj = {}
+    obj['full_name'] = user.full_name
+    obj['username'] = user.username
+    obj['email'] = user.email
+    obj['password'] = hash_password(user.password)
+    obj['type'] = user.type
+    obj['gender'] = user.gender
+    obj['bio'] = user.bio
+    obj['profile_image'] = user.profile_image
+    obj['created_at'] = user.created_at
+    obj['updated_at'] = user.updated_at
+    return obj
+
+
+def modify_user(json, user):
+    user.full_name = json['full_name'] if json['full_name'] else user.full_name
+    user.username = json['username'] if json['username'] else user.username
+    user.email = json['email'] if json['email'] else user.email
+    user.password = hash_password(json['password']) if json['password'] else user.password
+    user.type = json['type'] if json['type'] else user.type
+    user.gender = json['gender'] if json['gender'] else user.gender
+    user.bio = json['bio'] if json['bio'] else user.bio
+    user.profile_image = json['profile_image'] if json['profile_image'] else user.bio
+    user.updated_at = datetime.now()
+    return user
 
 
 @csrf_exempt
@@ -65,3 +94,63 @@ def logout(request):
         authentication.logout(token)
         return JsonResponse({'response': True})
     return JsonResponse({'response': False})
+
+
+@csrf_exempt
+def get_user(request, user_id):
+    if request.method == 'GET':
+        token = request.META.get('HTTP_AUTHORIZATION', None)
+        if token and authentication.is_authenticated(token):
+            user = User.objects.get(id=user_id)
+            try:
+                return JsonResponse({"response": True, "user": user_json(user)})
+            except Exception as e:
+                return JsonResponse({'response': False, 'error': str(e)})
+        else:
+            return JsonResponse({"response": False, "error": "Unauthorized"})
+    return JsonResponse({
+        "response": False,
+        "error": "wrong request method"
+    })
+
+
+@csrf_exempt
+def get_current_user(request):
+    if request.method == 'GET':
+        token = request.META.get('HTTP_AUTHORIZATION', None)
+        user_id = authentication.get_user_id(token)
+        if token and authentication.is_authenticated(token):
+            user = User.objects.get(id=user_id)
+            try:
+                return JsonResponse({"response": True, "user": user_json(user)})
+            except Exception as e:
+                return JsonResponse({'response': False, 'error': str(e)})
+        else:
+            return JsonResponse({"response": False, "error": "Unauthorized"})
+    return JsonResponse({
+        "response": False,
+        "error": "wrong request method"
+    })
+
+
+@csrf_exempt
+def update_user(request):
+    if request.method == 'POST':
+        token = request.META.get('HTTP_AUTHORIZATION', None)
+        body = json.loads(request.body.decode('utf-8'))
+        user_id = authentication.get_user_id(token)
+        user = User.objects.get(id=user_id)
+        modify_user(body, user)
+        if token and authentication.is_authenticated(token):
+            try:
+                user.save()
+                return JsonResponse({"response": True})
+            except Exception as e:
+                return JsonResponse({'response': False, 'error': str(e)})
+        else:
+            return JsonResponse({"response": False, "error": "Unauthorized"})
+    return JsonResponse({
+        "response": False,
+        "error": request.method
+    })
+
