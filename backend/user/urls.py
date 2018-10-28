d029359ac1819bed9ea9804b28afc7b14b257c14
alpkaanusen <ausen96@hotmail.com>
2018-10-29 00:32:00 +0300

    fixed minor bug in modify_user

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^auth/register', views.register, name="register"),
     url(r'^auth/logout', views.logout, name="logout"),
     url(r'^profile/(?P<user_id>[a-z0-9,]*)/$', views.get_user, name="get_user"),
-    url(r'^profile/$', views.get_current_user, name="get_current_user"),
+    url(r'^profile$', views.get_current_user, name="get_current_user"),
     url(r'^profile/update', views.update_user, name="update_user")
 ]
