3468d4e55b5c6d365a56cae9b2282a7095801159
Bahadir Oncel <bahadir.onc@gmail.com>
2018-10-28 16:00:32 +0300

    add email/password validation to login/signup screens, add copyright text to welcome screen, add extensions, add folders Helper and Extensions

--- android/app/src/main/java/com/karpuz/karpuz/Activity/LoginActivity.kt
+++ android/app/src/main/java/com/karpuz/karpuz/Activity/LoginActivity.kt
@@ -3,7 +3,9 @@ package com.karpuz.karpuz.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.karpuz.karpuz.Extensions.*
 import com.karpuz.karpuz.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -13,10 +15,24 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun signInClicked(button: View) {
-
+        val email = editText_login_email.text
+        if (!email.isValidEmail()) {
+            editText_login_email.errorState()
+            shortToast("Email address is not valid!")
+            return
+        }
+        editText_login_email.clearErrorState()
+        //TODO login
     }
 
     fun forgotPasswordClicked(button: View) {
-
+        val email = editText_login_email.text
+        if (!email.isValidEmail()) {
+            editText_login_email.errorState()
+            shortToast("Email address is not valid!")
+        }
+        editText_login_email.clearErrorState()
+        longToast("Password reset email sent to $email")
+        //TODO send reset password email
     }
 }
--- android/app/src/main/java/com/karpuz/karpuz/Activity/SignupActivity.kt
+++ android/app/src/main/java/com/karpuz/karpuz/Activity/SignupActivity.kt
@@ -3,7 +3,13 @@ package com.karpuz.karpuz.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.karpuz.karpuz.Extensions.errorState
 import com.karpuz.karpuz.R
+import com.karpuz.karpuz.Extensions.isValidEmail
+import com.karpuz.karpuz.Extensions.clearErrorState
+import com.karpuz.karpuz.Extensions.shortToast
+import kotlinx.android.synthetic.main.activity_signup.*
+
 
 class SignupActivity : AppCompatActivity() {
 
@@ -13,6 +19,27 @@ class SignupActivity : AppCompatActivity() {
     }
 
     fun registerClicked(button: View) {
-
+        val emailAddress = editText_signup_email.text
+        if (!emailAddress.isValidEmail()) {
+            editText_signup_email.errorState()
+            shortToast("Email is not valid!")
+            return
+        }
+        editText_signup_email.clearErrorState()
+        val password = editText_signup_password.text
+        if (password.length < 8) {
+            editText_signup_password.errorState()
+            shortToast("Passwords should be at least 8 characters!")
+            return
+        }
+        editText_signup_password.clearErrorState()
+        val passwordRepeat = editText_signup_repeat_password.text
+        if (password.toString() != passwordRepeat.toString()) {
+            editText_signup_repeat_password.errorState()
+            shortToast("Passwords do not match!")
+            return
+        }
+        editText_signup_repeat_password.clearErrorState()
+        // TODO register
     }
 }
