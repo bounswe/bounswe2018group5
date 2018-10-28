0bb67eebe348fd3b0d5e4fd7fcc17bdc2fe83989
eminayar <m.eminayar06@gmail.com>
2018-10-28 23:36:07 +0300

    timeout=100mins: for easier development

--- backend/user/authentication.py
+++ backend/user/authentication.py
@@ -15,7 +15,7 @@ def generate_token(user):
 def is_authenticated(token):
     global myMap
     previous_req = myMap[token] if token in myMap else 0
-    if (int(datetime.now().timestamp()) - previous_req) > 600:
+    if (int(datetime.now().timestamp()) - previous_req) > 6000:
         if token in myMap:
             del myMap[token]
         return False
