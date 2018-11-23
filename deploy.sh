632a2c950d297fe802b826e0aee8ef24842e9f63
Enis Simsar <enis.simsar@boun.edu.tr>
2018-11-23 20:54:09 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,8 @@ cp ./backend/.env .env
 docker-compose -f frontend/docker-compose.yml  down
 docker-compose -f backend/docker-compose.yml down
 
+docker-compose -f frontend/docker-compose.yml  build
+docker-compose -f backend/docker-compose.yml build
+
 docker-compose -f frontend/docker-compose.yml up -d
 docker-compose -f backend/docker-compose.yml up -d
