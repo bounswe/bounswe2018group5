a7eab7e82c0cf3143ceedf9079c785f88f5f8cbc
alpkaanusen <ausen96@hotmail.com>
2019-01-01 21:57:06 +0300

    Modifications to text_search endpoint

--- backend/project/views.py
+++ backend/project/views.py
@@ -4,6 +4,7 @@ from user import authentication, models, views
 from datetime import datetime
 from api.utils import *
 from api.semantic_tags import create_tag
+from django.db.models import Q
 
 import json
 from .models import *
@@ -96,19 +97,16 @@ def search_projects(request):
     if request.method == 'GET':
         query = request.GET.get('query', '')
         token = request.META.get('HTTP_AUTHORIZATION', None)
-        if token and authentication.is_authenticated(token):
-            user_id = authentication.get_user_id(token)
-            projects = Project.objects.search_text(query).filter(status__gte = 0)\
-                .order_by('$text_score')  # excludes discarded projects
-            res = []
-            for project in projects:
-                res.append(project_json(project,user_id))
-            try:
-                return JsonResponse({"response": True, "projects": res})
-            except Exception as e:
-                return JsonResponse({'response': False, 'error': str(e)})
-        else:
-            return JsonResponse({"response": False, "error": "Unauthorized"})
+        user_id = (authentication.get_user_id(token) if token else None)
+        projects = Project.objects.filter(Q(status__gte=0) &
+                                          (Q(title__icontains=query) | Q(description__icontains=query)))
+        res = []
+        for project in projects:
+            res.append(project_json(project, user_id))
+        try:
+            return JsonResponse({"response": True, "projects": res})
+        except Exception as e:
+            return JsonResponse({'response': False, 'error': str(e)})
     return JsonResponse({
         "response": False,
         "error": "wrong request method"
@@ -124,10 +122,10 @@ def project_handler(request):
         user_id = (authentication.get_user_id(token) if token else None)
         if ids:
             ids = ids.split(',')
-            projects = Project.objects.filter(id__in=ids) # TODO: order as given id list
+            projects = Project.objects.filter(id__in=ids)  # TODO: order as given id list
             res = []
             for project in projects:
-                res.append(project_json(project,user_id))
+                res.append(project_json(project, user_id))
             try:
                 return JsonResponse({"response": True, "projects": res})
             except Exception as e:
